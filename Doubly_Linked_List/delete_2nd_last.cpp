bd8df369e3f7c1a727a94546a993e2f4f9a22521
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-07 22:32:07 +0530

    delete

---
+++ Doubly_Linked_List/delete_2nd_last.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *prev,*next;
+};
+
+Node *first,*temp,*ttemp,*P;
+
+void create_first(int x){
+    first=new Node;
+    first->data=x;
+    first->next=first->prev=NULL;
+}
+
+void display(){
+    temp=first;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+void add_node(int x){
+    temp=first;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    ttemp=new Node;
+    ttemp->prev=temp;
+    ttemp->data=x;
+    ttemp->next=NULL;
+    temp->next=ttemp;
+}
+
+void delete_2nd_last(){
+    temp=first;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    ttemp=temp->prev;
+    P=ttemp->prev;
+    ttemp->next=temp->prev=NULL;
+    P->next=temp;
+    temp->prev=P;
+    delete ttemp;
+}
+
+int main(){
+    create_first(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    add_node(60);
+    display();
+    cout<<"After deletion : \n";
+    delete_2nd_last();
+    display();
+    return 0;
+}
---
+++ Doubly_Linked_List/delete_before.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *prev,*next;
+};
+
+Node *first,*temp,*ttemp,*P;
+
+void create_first(int x){
+    first=new Node;
+    first->data=x;
+    first->next=first->prev=NULL;
+}
+
+void display(){
+    temp=first;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+void add_node(int x){
+    temp=first;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    ttemp=new Node;
+    ttemp->prev=temp;
+    ttemp->data=x;
+    ttemp->next=NULL;
+    temp->next=ttemp;
+}
+
+void delete_before(int x){
+    temp=first;
+    while(temp->data!=x){
+        temp=temp->next;
+    }
+    ttemp=temp->prev;
+    P=ttemp->prev;
+    P->next=temp;
+    ttemp->next=ttemp->prev=NULL;
+    temp->prev=P;
+    delete ttemp;
+}
+
+int main(){
+    create_first(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    add_node(60);
+    display();
+    cout<<"after deleting node : \n";
+    delete_before(50);
+    display();
+    return 0;
+}
