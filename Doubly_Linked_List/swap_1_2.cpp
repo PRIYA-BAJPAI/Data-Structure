2fa044f13e13e1115cd73e525f048f083814c781
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-08 09:57:22 +0530

    swap

---
+++ Doubly_Linked_List/swap_1_2.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *prev,*next;
+};
+
+Node *first,*temp,*ttemp,*P;
+
+void create_first(int x){
+    first=new Node;
+    first->data=x;
+    first->next=first->prev=NULL;
+}
+
+void display(){
+    temp=first;
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+void add_node(int x){
+    temp=first;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    ttemp=new Node;
+    ttemp->prev=temp;
+    ttemp->data=x;
+    ttemp->next=NULL;
+    temp->next=ttemp;
+}
+
+void swap_1st_2nd(){
+    temp=first->next;
+    ttemp=temp->next;
+    first->next=ttemp;
+    first->prev=temp;
+    temp->next=first;
+    temp->prev=NULL;
+    ttemp->prev=first;
+    first=temp;
+}
+
+int main(){
+    create_first(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    add_node(60);
+    display();
+    cout<<"After swapping :"<<endl;
+    swap_1st_2nd();
+    display();
+    return 0;
+}
