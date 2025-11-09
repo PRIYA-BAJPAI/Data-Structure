c42829925b42a65f8eb3e4e4df2c996dccda59ab
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-09 11:10:22 +0530

    swap

---
+++ Doubly_Circular_LL/delete_last.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next,*prev;
+};
+
+Node *first,*temp,*ttemp,*P;
+
+void create_first(int x){
+    first=new Node;
+    first->next=first->prev=first;
+    first->data=x;
+}
+
+void display(){
+    temp=first;
+    do{
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }while(temp!=first);
+}
+
+void add_node(int x){
+    temp=first->prev;
+    ttemp=new Node;
+    ttemp->data=x;
+    ttemp->next=first;
+    ttemp->prev=temp;
+    temp->next=ttemp;
+    first->prev=ttemp;
+}
+
+void delete_last(){
+    temp=first->prev;
+    ttemp=temp->prev;
+    temp->next=temp->prev=NULL;
+    ttemp->next=first;
+    first->prev=ttemp;
+    delete temp;
+}
+
+int main(){
+    create_first(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    add_node(60);
+    add_node(70);
+    display();
+    cout<<"After deletion :"<<endl;
+    delete_last();
+    display();
+}
---
+++ Doubly_Circular_LL/swapping_1st_2nd.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next,*prev;
+};
+
+Node *first,*temp,*ttemp,*P;
+
+void create_first(int x){
+    first=new Node;
+    first->next=first->prev=first;
+    first->data=x;
+}
+
+void display(){
+    temp=first;
+    do{
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }while(temp!=first);
+}
+
+void add_node(int x){
+    temp=first->prev;
+    ttemp=new Node;
+    ttemp->data=x;
+    ttemp->next=first;
+    ttemp->prev=temp;
+    temp->next=ttemp;
+    first->prev=ttemp;
+}
+
+void swap_1st_2nd(){
+    temp=first->next;
+    ttemp=temp->next;
+    P=first->prev;
+    first->next=ttemp;
+    first->prev=temp;
+    temp->prev=P;
+    temp->next=first;
+    ttemp->prev=first;
+    P->next=temp;
+    first=temp;
+}
+
+int main(){
+    create_first(10);
+    add_node(20);
+    add_node(30);
+    add_node(40);
+    add_node(50);
+    add_node(60);
+    add_node(70);
+    display();
+    cout<<"After swapping :"<<endl;
+    swap_1st_2nd();
+    display();
+}
