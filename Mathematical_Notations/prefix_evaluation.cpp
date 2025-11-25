b77d73be781bc4afeccbdb8f348782cc49169a21
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-25 16:16:03 +0530

    evaluation

---
+++ Mathematical_Notations/prefix_evaluation.cpp
@@ -0,0 +1,158 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+#include<stack>
+#include<string>
+using namespace std;
+
+struct notation{
+    string data;
+    notation *next;
+}; 
+
+notation *top,*temp,*ttemp;
+
+void init(){
+    top=temp=ttemp=NULL;
+}
+
+//precidence
+int prioprity_operator(char ch){
+    if(ch=='^') return 3;
+    if(ch=='*' || ch=='/') return 2;
+    if(ch=='+' || ch=='-') return 1;
+    return 0;
+
+}
+
+bool isOperand(char ch){
+    if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+        return true;
+    }
+    return false;
+}
+
+void push(string x){
+    temp=new notation;
+    if(temp==NULL){
+        cout<<"Stack is full";
+        return ;
+    }
+    temp->data=x;
+    temp->next=top;
+    top=temp;
+}
+
+string pop(){
+    string x;
+    if(top==NULL){
+        return 0;
+     }
+    temp=top;
+    x=temp->data;
+    top=top->next;
+    delete temp;
+    return x;
+}
+
+string stack_top(){
+    if(top==NULL){
+        return "";
+    }
+    return top->data;
+}
+
+void infix_prefix(char infix[],string prefix[]){
+    init();
+    int i=0,j=0;
+    string str1,str2,str3,str4,str5;
+    while(infix[i]!='\0'){
+        if(isOperand(infix[i])){
+            prefix[j++]=string(1,infix[i]); //string of length 1 having data infix[i]
+        }else if(infix[i]=='('){
+            push("(");
+        }else if(infix[i]==')'){
+            while(top!=NULL && stack_top()!="("){
+                str1=prefix[j-1];
+                str2=prefix[j-2];
+                j=j-2;
+                str3=pop();
+                str4=str2+str1;
+                str5=str3+str4;
+                prefix[j++]=str5;
+            }
+            pop(); //to remove (
+        }
+        else{
+            while(top!=NULL && prioprity_operator(stack_top()[0])>=prioprity_operator(infix[i])){
+                str1=prefix[j-1];
+                str2=prefix[j-2];
+                j=j-2;
+                str3=pop();
+                str4=str2+str1;
+                str5=str3+str4;
+                prefix[j++]=str5;
+            }
+            push(string(1,infix[i]));
+        }
+        i++; //always remember
+    }
+    //pop remaining elements
+    while(top!=NULL){
+        str1=prefix[j-1];
+        str2=prefix[j-2];
+        j=j-2;
+        str3=pop();
+        str4=str2+str1;
+        str5=str3+str4;
+        prefix[j++]=str5;
+    }
+    prefix[j]=""; //last element (empty)
+}
+
+void post_evaluation(string prefix[]){
+    
+    int i;
+    //notation s;
+    int OP1,OP2,R;
+    int calculate(int,int ,char);
+    for(i=0;i<strlen(prefix);i++){
+        if(isalnum(prefix[i])){
+            push(prefix[i]);
+        }else{
+            OP1=pop();
+            OP2=pop();
+            R=calculate(OP1,OP2,prefix[i]);
+            push(R);
+        }
+    }
+    cout<<"result : "<<pop();
+}
+
+int calculate(int O1,int O2,char x){
+    switch(x){
+        case '+':return O1+O2;
+        case '-':return O1-O2;
+        case '*':return O1*O2;
+        case '/':return O1/O2;
+        case '^':return pow(O1,O2);
+        default:return -1;
+    }
+}
+
+
+int main(){
+    char infix[50];
+    string prefix[50];
+    cout<<"Enter Infix Notation :";
+    cin>>infix;
+    
+    cout<<"prefix :";
+    infix_prefix(infix,prefix);
+    for(int k=0;prefix[k] !="";k++){
+        cout<<prefix[k];
+    }
+   
+    return 0;
+}
+
