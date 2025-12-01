96d15d3ce48d6290a561db198f10a313e6c10352
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-12-01 21:43:05 +0530

    BST

---
+++ BinarySearchTree/preorder.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+struct BST{
+    int data;
+    BST *left;
+    BST *right;
+};
+BST *root,*temp,*ttemp,*P;
+void init(){
+    root=temp=ttemp=P=NULL;
+}
+void create_root(int x){
+    root=new BST;
+    root->data=x;
+    root->left=root->right=NULL;
+}
+void add_node(int x){
+    temp=root;
+    while(temp!=NULL){
+        ttemp=temp;
+        if(temp->data>x){
+            temp=temp->left;
+        }else{
+            temp=temp->right;
+        }
+    }
+    P=new BST;
+    P->data=x;
+    P->left=P->right=NULL;
+    if(ttemp->data>x){
+        ttemp->left=P;
+    }else{
+        ttemp->right=P;
+    }
+}
+void preorder(BST *P){
+    if(P!=NULL){
+        cout<<P->data<<" ";
+        preorder(P->left);
+        preorder(P->right);
+    }
+}
+int main(){
+    init();
+    create_root(100);
+    add_node(50);
+    add_node(200);
+    add_node(10);
+    add_node(70);
+    add_node(150);
+    add_node(300);
+    add_node(2);
+    add_node(30);
+    add_node(65);
+    add_node(90);
+    add_node(180);
+    add_node(15);
+    add_node(150);
+    add_node(20);
+    
+    preorder(root);
+}
