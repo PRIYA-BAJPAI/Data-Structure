c7aebee44fc355c165425cb757d63a60c7f59208
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-12-08 21:47:06 +0530

    width of a binary search tree

---
+++ BinarySearchTree/width.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+struct BST{
+    int data;
+    BST *left;
+    BST *right;
+};
+BST *root,*temp,*ttemp,*P;
+void init(){
+    root=temp=ttemp=P=NULL;
+}
+void create_root(int x){
+    root=new BST;
+    root->data=x;
+    root->left=root->right=NULL;
+}
+void add_node(int x){
+    temp=root;
+    while(temp!=NULL){
+        ttemp=temp;
+        if(temp->data>x){
+            temp=temp->left;
+        }else{
+            temp=temp->right;
+        }
+    }
+    P=new BST;
+    P->data=x;
+    P->left=P->right=NULL;
+    if(ttemp->data>x){
+        ttemp->left=P;
+    }else{
+        ttemp->right=P;
+    }
+}
+
+int widthAtLevel(BST *root,int level){
+    if(root==NULL){
+        return 0;
+    }
+    if(level==0){
+        return 1;
+    }
+    return widthAtLevel(root->left,level-1) + widthAtLevel(root->right,level-1);
+}
+
+int main(){
+    init();
+    create_root(100);
+    add_node(50);
+    add_node(200);
+    add_node(10);
+    add_node(70);
+    add_node(150);
+    add_node(300);
+    add_node(2);
+    add_node(30);
+    add_node(65);
+    add_node(90);
+    add_node(180);
+    add_node(15);
+    add_node(150);
+    add_node(20);
+    cout<<"Width is : "<<widthAtLevel(root,3);
+}
